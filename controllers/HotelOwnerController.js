5003fe4cf83cea0c1f8803a8206be3a7e4f060cc
lkmSasanga <lkmsasanga@gmail.com>
2021-04-25 19:14:44 +0530

    getBooking method added in hotelOwner controller

--- controllers/HotelOwnerController.js
+++ controllers/HotelOwnerController.js
@@ -4,6 +4,30 @@ const { Booking } = require("../models/BookingModel");
 // find bookings relevent to the hotel HotelOwner
 // need to add hotel owner id into booking
 
+exports.getBookings = async(req, res) => {
+    await Boking.findById({ 'hotelOwner_id': req.params.id }, async function(err, booking) {
+        if (err) {
+            return res.status(422).json({
+                success: false,
+                message: "Invalid booking id!"
+            });
+        }
+
+        if (!booking) {
+            return res.status(422).json({
+                success: false,
+                message: "Invalid booking id!"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "cBookings received!",
+            data: booking
+        });
+    });
+};
+
 exports.addHotel = async(req, res) => {
     let newHotel = new Hotel(req.body);
 
