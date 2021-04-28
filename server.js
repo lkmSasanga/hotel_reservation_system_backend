a0273e544b2ad9127b6eb0981bb9e623266be59f
lkmSasanga <lkmsasanga@gmail.com>
2021-04-28 21:11:28 +0530

    file size edited to 50mb

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors());
-app.use(express.json())
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
 
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
