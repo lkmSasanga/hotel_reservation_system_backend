0ea5b4a56abcc56eaee2115cc1024b8bd6fa10b6
lkmSasanga <lkmsasanga@gmail.com>
2021-04-30 15:36:06 +0530

    req query added to getHotels  controller

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -22,9 +22,10 @@ exports.getTowns = async(req, res) => {
     });
 };
 
-
+//req.params.city
 exports.getHotels = async(req, res) => {
-    await Hotel.find({ "city": req.params.town }, async function(err, hotel) {
+    await Hotel.find({ "city": `${req.query.city}` }, async function(err, hotels) {
+        console.log(`${req.query.city}`);
         if (err) {
             return res.status(422).json({
                 success: false,
@@ -32,19 +33,35 @@ exports.getHotels = async(req, res) => {
             });
         }
 
-        if (!hotel) {
+        if (!hotels) {
             return res.status(422).json({
                 success: false,
                 message: "Invalid City!"
             });
         }
 
-        return res.status(422).json({
+        return res.status(200).json({
             success: true,
             message: "Hotels received!",
-            data: hotel
+            data: hotels
         });
     });
+    // await Hotel.find(function(err, hotels) {
+    //     if (err) {
+    //         return res.status(422).json({
+    //             success: false,
+    //             message: "Unable to retrieve hotels!",
+    //             data: err
+    //         });
+    //     }
+
+    //     return res.status(200).json({
+    //         success: true,
+    //         message: "Received hotels!",
+    //         data: hotels
+    //     });
+    // });
+
 };
 
 exports.addBooking = async(req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 let express = require('express');
 let router = express.Router();
 
-// let Hotel = require('../models/hotelModel')
+// let Town = require('../models/TownModel');
 
 router.get('/api', function(req, res) {
     res.send("Welcome to API!");
 });
 
 // end point localhost:5000/
-// router.route('/hotels').get((req, res) => {
-//     Hotel.find()
-//         .then(hotel => res.json(hotel))
+// router.route('/towns').get((req, res) => {
+//     Town.find()
+//         .then(towns => res.json(towns))
 //         .catch(err => res.status(400).json('Error: ' + err))
 
 // })
