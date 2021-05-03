46df88f53103ea6b5a224ae85fca0150021fce8a
lkmSasanga <lkmsasanga@gmail.com>
2021-05-03 12:10:43 +0530

    payment.js file added in seperate file

--- server.js
+++ server.js
@@ -33,52 +33,52 @@ app.use(function(req, res) {
 });
 
 // payment gateway: stripe
-app.get("/", (req, res) => {
-    res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
-});
+// app.get("/payment", (req, res) => {
+//     res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
+// });
 
-app.post("/checkout", async(req, res) => {
-    console.log("Request:", req.body);
+// app.post("/checkout", async(req, res) => {
+//     console.log("Request:", req.body);
 
-    let error;
-    let status;
-    try {
-        const { token, bookingDetails } = req.body;
+//     let error;
+//     let status;
+//     try {
+//         const { token, bookingDetails } = req.body;
 
-        const customer = await stripe.customers.create({
-            email: token.email,
-            source: token.id
-        });
+//         const customer = await stripe.customers.create({
+//             email: token.email,
+//             source: token.id
+//         });
 
-        const idempotency_key = uuid();
-        const charge = await stripe.charges.create({
-            amount: bookingDetails.price * 100,
-            currency: "usd",
-            customer: customer.id,
-            receipt_email: token.email,
-            description: `Purchased the ${bookingDetails.hotel_name}`,
-            shipping: {
-                name: token.card.name,
-                address: {
-                    line1: token.card.address_line1,
-                    line2: token.card.address_line2,
-                    city: token.card.address_city,
-                    country: token.card.address_country,
-                    postal_code: token.card.address_zip
-                }
-            }
-        }, {
-            idempotency_key
-        });
-        console.log("Charge:", { charge });
-        status = "success";
-    } catch (error) {
-        console.error("Error:", error);
-        status = "failure";
-    }
+//         const idempotency_key = uuid();
+//         const charge = await stripe.charges.create({
+//             amount: bookingDetails.price * 100,
+//             currency: "usd",
+//             customer: customer.id,
+//             receipt_email: token.email,
+//             description: `Purchased the ${bookingDetails.hotel_name}`,
+//             // shipping: {
+//             //     name: token.card.name,
+//             //     address: {
+//             //         line1: token.card.address_line1,
+//             //         line2: token.card.address_line2,
+//             //         city: token.card.address_city,
+//             //         country: token.card.address_country,
+//             //         postal_code: token.card.address_zip
+//             //     }
+//             // }
+//         }, {
+//             idempotency_key
+//         });
+//         console.log("Charge:", { charge });
+//         status = "success";
+//     } catch (error) {
+//         console.error("Error:", error);
+//         status = "failure";
+//     }
 
-    res.json({ error, status });
-});
+//     res.json({ error, status });
+// });
 
 
 app.listen(port, () => {
