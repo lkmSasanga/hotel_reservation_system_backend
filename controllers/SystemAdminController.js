f40a8c75bb26dc6da58cea0d06d701fce2b0e772
lkmSasanga <lkmsasanga@gmail.com>
2021-05-04 15:59:09 +0530

    get all hotel oweners route added to SA

--- controllers/SystemAdminController.js
+++ controllers/SystemAdminController.js
@@ -49,3 +49,27 @@ exports.getCustomers = async(req, res) => {
         });
     });
 };
+
+exports.getHotelOwners = async(req, res) => {
+    await User.find({ "userType": `${req.query.userType}` }, async function(err, user) {
+        if (err) {
+            return res.status(422).json({
+                success: false,
+                message: "Invalid user type!"
+            });
+        }
+
+        if (!user) {
+            return res.status(422).json({
+                success: false,
+                message: "Invalid user type!"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Customers received!",
+            data: user
+        });
+    });
+};
--- routes/SystemAdminRoutes.js
+++ routes/SystemAdminRoutes.js
@@ -6,6 +6,6 @@ module.exports = function(app) {
 
     app.post("/api/add_town", [Auth, SystemAdmin], SystemAdminController.addTown);
     app.get("/api/all_customers", [Auth, SystemAdmin], SystemAdminController.getCustomers);
-    // app.get("/api/all_hotel_owners", [Auth, SystemAdmin], SystemAdminController.getHotelOwners);
+    app.get("/api/all_hotel_owners", [Auth, SystemAdmin], SystemAdminController.getHotelOwners);
 
 };
